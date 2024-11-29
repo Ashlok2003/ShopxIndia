140a2e6596ecbeac529523724539132786463884
Ashlok2003 <chaudharyashlok@gmail.com>
2024-11-29 19:50:20 +0530

    Initial commit

---
+++ test/shopxindia.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Shopxindia from '../lib/shopxindia-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shopxindia-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Shopxindia.ShopxindiaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
